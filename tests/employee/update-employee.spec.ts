735728beaa633c30ff100a881c6d9f273913a077
GdPaul1234 <paul.godin1234@outlook.fr>
2023-01-03 15:59:18 +0100

    Add update employee tests

---
+++ tests/employee/update-employee.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect, Page } from '@playwright/test';
+import { fillOutForm, EmployeeData, new_employee_data } from './employee-helper';
+
+type Input = {
+    key: keyof EmployeeData
+    value: string
+}
+
+async function updateEmployee(page: Page, linkName: string, inputs: Input[]) {
+    await page.getByRole('link', { name: linkName }).click()
+
+    for (const { key, value } of inputs) {
+        await page.fill(`#id_${key}`, value)
+    }
+    await page.getByRole('button', { name: 'Update' }).click()
+}
+
+async function assertEmployee(page: Page, linkName: string, expected: Input[]) {
+    await page.getByRole('link', { name: linkName }).click()
+
+    for (const { key, value } of expected) {
+        expect(await page.locator(`#id_${key}`).inputValue()).toEqual(value)
+    }
+}
+
+test.describe('Update employee', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://x.hr.dmerej.info/add_employee');
+        await fillOutForm(new_employee_data, page)
+        await page.getByRole('button', { name: 'Add' }).click()
+
+        await page.goto('https://x.hr.dmerej.info/employees')
+        await page.getByText('Edit').first().click() // edit the first employee
+    })
+
+    test.afterEach(async ({ page }) => {
+        await page.goto('https://x.hr.dmerej.info/reset_db')
+        await page.getByText('Proceed').click()
+    })
+
+    test.describe('Update address', () => {
+        test('Update Address 1 and Address 2', async ({ page }) => {
+            const linkName = ' Update address '
+            const inputs: Input[] = [
+                { key: 'address_line1', value: 'Fake Address 1' },
+                { key: 'address_line2', value: 'Fake Address 2' }
+            ]
+
+            await updateEmployee(page, linkName, inputs)
+            await assertEmployee(page, linkName, inputs)
+        })
+
+        test('Update City', async ({ page }) => {
+            const linkName = ' Update address '
+            const inputs: Input[] = [{ key: 'city', value: 'New City' }]
+
+            await updateEmployee(page, linkName, inputs)
+            await assertEmployee(page, linkName, inputs)
+        })
+    })
+
+    test.describe('Update contract', () => {
+        test('should persist update', async ({ page }) => {
+            const linkName = ' Update contract '
+            const inputs: Input[] = [{ key: 'job_title', value: 'New job' }]
+
+            await updateEmployee(page, linkName, inputs)
+            await assertEmployee(page, linkName, inputs)
+        })
+    })
+})
